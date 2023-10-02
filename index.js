5085997abdb9950199cfc14ecc56e45d7d8b4300
elin57 <112911997+elin57@users.noreply.github.com>
2023-10-02 11:45:40 +0200

    Timer work
    More work has been done on the timer. Time can now be incremented, and the incrementation is shown on the page.

--- index.js
+++ index.js
@@ -1,44 +1,39 @@
 "use strict";
 
-// Set the date we're counting down to
 let months = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
 let daysInEachMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 
-console.log(months[new Date().getMonth() + 1]);
-let month = months[new Date().getMonth()];
-let date = new Date().getDate();
-let year = new Date().getFullYear();
-let hour = new Date().getHours();
-let minutes = new Date().getMinutes();
-let seconds = new Date().getSeconds();
+let x;
+let month;
+let year;
+let date;
+let hour;
+let minutes;
+let seconds;
+let addedSeconds = 0;
+let addedMinutes = 0;
+let countDownDate;
 
-let addedSeconds = 20;
-let addedMinutes = 4;
-console.log(minutes);
-console.log(seconds);
 
-addSeconds();
-addMinutes();
 setEggTimer();
 
-console.log(addedMinutes);
-console.log(minutes);
-console.log(seconds);
-
-
-function addSeconds() {
+function addSeconds(seconds) {
     seconds = seconds + addedSeconds;
     console.log(seconds);
     if(seconds > 59) {
-        addedMinutes += 1;
+        minutes += 1;
         let extraSeconds = seconds - 59;
         seconds = extraSeconds;
-        console.log(addedMinutes);
+        console.log(extraSeconds);
+        console.log(seconds);
+    } else {
+        seconds = seconds + 1;
     }
+    return seconds;
 }
 
-function addMinutes() {
+function addMinutes(minutes, hour) {
     console.log(minutes);
     for(let i = 0; i < addedMinutes; i++) {
         minutes = minutes + 1;
@@ -61,36 +56,104 @@ function addMinutes() {
             }
         }
     }
-    console.log(minutes);
+    let values = {
+        minutes: minutes,
+        hour: hour
+    }
+    return values;
 }
 
-var countDownDate;
-function setEggTimer() {
-    countDownDate = new Date(`${month} ${date}, ${year} ${hour}:${minutes}:${seconds}`).getTime();
-    document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
+function setEggTimer(incrementMinutes) {
+    let interval;
+    if(incrementMinutes !== undefined) {
+        let stay = true;
+        let totalMinutes = addedMinutes + incrementMinutes;
+        let initialMinutes = totalMinutes - incrementMinutes;
+        //document.getElementById("timer").innerHTML = initialMinutes + "m " + addedSeconds + "s ";
+        console.log(addedMinutes);
+        console.log(totalMinutes);
+        let i = addedMinutes + 1;
+        interval = setInterval(function() {
+            document.getElementById("timer").innerHTML = i + "m " + addedSeconds + "s ";
+            if(i === totalMinutes) {
+                clearInterval(interval);
+            }
+            i++;
+            
+        }, 300);
+                
+        console.log(interval);
+        addedMinutes += incrementMinutes;
+    } else {
+        document.getElementById("timer").innerHTML = addedMinutes + "m " + addedSeconds + "s ";
+    }
 }
 
 // Update the count down every 1 second
-var x = setInterval(function() {
+let countDown = () => {
+    let now = new Date().getTime();
 
-  // Get today's date and time
-  var now = new Date().getTime();
+    // Find the distance between now and the count down date
+    let distance = countDownDate - now;
 
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+    minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
 
-  // Time calculations for days, hours, minutes and seconds
-  //var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  //var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("timer").innerHTML = "EXPIRED";
+    }
+}
 
-  // Display the result in the element with id="demo"
-  document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
+document.getElementById("startTimer").addEventListener("click", function() {
+    month = months[new Date().getMonth()];
+    date = new Date().getDate();
+    year = new Date().getFullYear();
+    hour = new Date().getHours();
+    minutes = new Date().getMinutes();
+    seconds = new Date().getSeconds();
 
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("timer").innerHTML = "EXPIRED";
-  }
-}, 1000);
+    let values = addMinutes(minutes, hour);
+    minutes = values.minutes;
+    hour = values.hour;
+    seconds = addSeconds(seconds);
+    console.log(hour);
+
+    console.log(seconds);
+    console.log(minutes);
+    console.log(hour);
+    countDownDate = new Date(`${month} ${date}, ${year} ${hour}:${minutes}:${seconds}`).getTime();
+    countDown();
+    x = setInterval(countDown, 1000);
+
+});
+
+let div = document.createElement("div");
+document.querySelector("body").appendChild(div);
+div.setAttribute("id", "questionContainer");
+div.innerHTML = 
+`
+<div>How do you want your eggs?</div>
+<div id=howDone>Soft</div><div id="howDone">Medium</div><div id="howDone">Hard</div>
+`;
+
+let howDone = document.querySelectorAll("div#howDone");
+
+for(let i = 0; i < howDone.length; i++) {
+    howDone[i].addEventListener("click", howBoiled);
+}
+
+function howBoiled(event) {
+    let incrementMinutes;
+    if(event.originalTarget.textContent === "Soft") {
+        incrementMinutes = 2;
+    } else if(event.originalTarget.textContent === "Medium") {
+        incrementMinutes = 3;
+    } else {
+        incrementMinutes = 4;
+    }
+
+   setEggTimer(incrementMinutes);
+}
